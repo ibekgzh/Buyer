1f880109774ad835c419a4c78fd40112a95835cb
Beksultan-Aksakalov <axakalov.b@gmail.com>
2023-05-23 23:35:01 +0600

    add profile

--- app/src/main/java/com/example/buyerapp/presenter/new_pincode/NewPinCodeViewModal.kt
+++ app/src/main/java/com/example/buyerapp/presenter/new_pincode/NewPinCodeViewModal.kt
@@ -2,11 +2,16 @@ package com.example.buyerapp.presenter.new_pincode
 
 import com.example.buyerapp.core.framework.mvi.MviViewModel
 import com.example.buyerapp.domain.usecase.AuthCompleteUseCase
+import com.example.buyerapp.domain.usecase.GetAuthKeyUseCase
+import com.example.buyerapp.domain.usecase.SaveAuthKeyUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class NewPinCodeViewModal @Inject constructor(private val onAuthCompleteUseCase: AuthCompleteUseCase) :
+class NewPinCodeViewModal @Inject constructor(
+    private val onAuthCompleteUseCase: AuthCompleteUseCase,
+    private val authKeySaveUseCase: SaveAuthKeyUseCase,
+) :
     MviViewModel<NewPinCodeViewState, NewPinCodeEvent, NewPinCodeEffect>() {
 
     override fun onTriggerEvent(eventType: NewPinCodeEvent) {
@@ -21,6 +26,9 @@ class NewPinCodeViewModal @Inject constructor(private val onAuthCompleteUseCase:
 
     private fun onComplete(smsToken: String, cellPhone: String, newPin: String) = safeLaunch {
         execute(onAuthCompleteUseCase(AuthCompleteUseCase.Params(smsToken, cellPhone, newPin))) {
+            safeLaunch {
+                call(authKeySaveUseCase(SaveAuthKeyUseCase.Params(it.key)))
+            }
             effectChannel.trySend(NewPinCodeEffect.OnNavigateHome)
         }
     }
