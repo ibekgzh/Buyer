73abb7d81eec9b2d9a3bcfacf73b46da43e156cc
Beksultan-Aksakalov <axakalov.b@gmail.com>
2023-05-30 02:38:29 +0600

    fixed shop screen content

--- app/src/main/java/com/example/buyerapp/presenter/shop/view/ShopScreenContent.kt
+++ app/src/main/java/com/example/buyerapp/presenter/shop/view/ShopScreenContent.kt
@@ -9,13 +9,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Surface
+import androidx.compose.material.Switch
+import androidx.compose.material.SwitchColors
 import androidx.compose.material.Text
+import androidx.compose.material3.Divider
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
@@ -37,37 +48,187 @@ import java.util.Locale
 @Composable
 fun ShopScreenContent() {
 
-    Column(
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        Boxes {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
-                    .background(Color.LightGray)
-                    .clip(shape = RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp))
-                    .paint(painterResource(id = R.drawable.shop))
-            )
-            Box(
-                modifier = Modifier
-                    .size(77.dp)
-                    .clip(shape = RoundedCornerShape(20.dp))
-                    .paint(painterResource(id = R.drawable.shop_logo))
-            )
-        }
-
-        Text(
-            text = "Marvin".uppercase(),
-            fontFamily = FontFamily.Default,
-            fontStyle = FontStyle.Normal,
-            fontWeight = FontWeight.W800,
-            fontSize = 24.sp,
-            lineHeight = 30.sp,
-            textAlign = TextAlign.Justify
-        )
+    var switchOn by remember {
+        mutableStateOf(false)
     }
 
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column {
+
+            Boxes(modifier = Modifier.fillMaxWidth()) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .background(Color.LightGray)
+                        .clip(shape = RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp))
+                        .paint(painterResource(id = R.drawable.shop))
+                )
+                Box(
+                    modifier = Modifier
+                        .size(77.dp)
+                        .clip(shape = RoundedCornerShape(20.dp))
+                        .paint(painterResource(id = R.drawable.shop_logo))
+                )
+            }
+
+            Column(modifier = Modifier.padding(start = 20.dp, end = 20.dp)) {
+                Text(
+                    text = "Marvin".uppercase(),
+                    fontFamily = FontFamily.Default,
+                    fontStyle = FontStyle.Normal,
+                    fontWeight = FontWeight.W800,
+                    fontSize = 24.sp,
+                    lineHeight = 30.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(top = 17.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
+
+                Text(
+                    text = "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+                    fontFamily = FontFamily.Default,
+                    fontStyle = FontStyle.Normal,
+                    fontWeight = FontWeight.W300,
+                    fontSize = 16.sp,
+                    lineHeight = 30.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(top = 11.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(start = 20.dp, end = 20.dp),
+                verticalArrangement = Arrangement.Bottom,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Divider(
+                    color = colorResource(id = R.color.gray2),
+                    thickness = 1.dp,
+                )
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 30.dp, bottom = 30.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Подписаться на уведомление",
+                        fontFamily = FontFamily.Default,
+                        fontStyle = FontStyle.Normal,
+                        fontWeight = FontWeight.W600,
+                        fontSize = 16.sp,
+                        lineHeight = 30.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
+
+                    Switch(
+                        checked = switchOn,
+                        onCheckedChange = { switchOn_ ->
+                            switchOn = switchOn_
+                        }
+                    )
+
+                }
+            }
+
+        }
+    }
+
+//    Box(
+//        modifier = Modifier.fillMaxSize()
+//    ) {
+//        Boxes {
+//            Box(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .height(200.dp)
+//                    .background(Color.LightGray)
+//                    .clip(shape = RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp))
+//                    .paint(painterResource(id = R.drawable.shop))
+//            )
+//            Box(
+//                modifier = Modifier
+//                    .size(77.dp)
+//                    .clip(shape = RoundedCornerShape(20.dp))
+//                    .paint(painterResource(id = R.drawable.shop_logo))
+//            )
+//        }
+//
+//        Box(
+//            modifier = Modifier
+//                .align(Alignment.Center)
+//                .padding(20.dp)
+//        ) {
+//            Text(
+//                text = "Marvin".uppercase(),
+//                fontFamily = FontFamily.Default,
+//                fontStyle = FontStyle.Normal,
+//                fontWeight = FontWeight.W800,
+//                fontSize = 24.sp,
+//                lineHeight = 30.sp,
+//                modifier = Modifier
+//                    .align(Alignment.TopCenter)
+//            )
+//
+//            Text(
+//                text = "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+//                fontFamily = FontFamily.Default,
+//                fontStyle = FontStyle.Normal,
+//                fontWeight = FontWeight.W300,
+//                fontSize = 16.sp,
+//                lineHeight = 30.sp,
+//                modifier = Modifier
+//                    .align(Alignment.Center)
+//            )
+//
+//        }
+//
+//        Box(
+//            modifier = Modifier
+//                .align(Alignment.BottomCenter)
+//                .padding(start = 20.dp, end = 20.dp, top = 40.dp, bottom = 40.dp)
+//        ) {
+//            Divider(
+//                color = colorResource(id = R.color.gray2),
+//                thickness = 1.dp,
+//            )
+//
+//            Row(
+//                modifier = Modifier.fillMaxWidth(),
+//                horizontalArrangement = Arrangement.SpaceBetween,
+//                verticalAlignment = Alignment.CenterVertically
+//            ) {
+//                Text(
+//                    text = "Подписаться на уведомление",
+//                    fontFamily = FontFamily.Default,
+//                    fontStyle = FontStyle.Normal,
+//                    fontWeight = FontWeight.W600,
+//                    fontSize = 16.sp,
+//                    lineHeight = 30.sp,
+//                    color = Color.Black,
+//                    textAlign = TextAlign.Center
+//                )
+//
+//                Switch(
+//                    checked = switchOn,
+//                    onCheckedChange = { switchOn_ ->
+//                        switchOn = switchOn_
+//                    }
+//                )
+//
+//            }
+//        }
+//
+//    }
 }
 
 @Composable
@@ -89,9 +250,12 @@ fun Boxes(
         val smallPlaceable = smallBox.measure(looseConstraints)
         layout(
             width = constraints.maxWidth,
-            height = constraints.maxHeight
+            height = largePlaceable.height + smallPlaceable.height / 2,
         ) {
-            largePlaceable.placeRelative(0, 0)
+            largePlaceable.placeRelative(
+                x = 0,
+                y = 0,
+            )
             smallPlaceable.placeRelative(
                 x = (constraints.maxWidth - smallPlaceable.width) / 2,
                 y = largePlaceable.height - smallPlaceable.height / 2
